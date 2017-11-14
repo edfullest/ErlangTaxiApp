853082f62d7ab9cb8e7d01633c8420c861c42059
itsStrobe <josezavala0297@gmail.com>
2017-11-14 01:50:51 -0600

    Client Functions Added. Review Needed

--- cliente.erl
+++ cliente.erl
@@ -1,21 +1,41 @@
 -module(cliente). 
--export([consulta/1, deposita/2, retira/2]).
-% nombre largo del servidor (nombre@máquina) 
-matriz() -> 'servidor@DESKTOP-HQ62L6T.local'. % funciones de interfase 
+-export([pedir_taxi/0]).
+-import('matriz', [genera_nodo/1]).
+
+% La funcion genera_nodo genera la matriz a donde se conectara, con todo y su hostname
+% En este caso, se genera servidor_taxi@HOSTNAME
+nodo_servidor() -> genera_nodo("servidor_taxi").
+nodo_taxi() -> genera_nodo("taxi").
 pedir_taxi(Quien, Location) ->
-    pide_taxi({pedir,Quien, Location}).
+    pide_taxi({pedir, Quien, Location}).
 
 % cliente 
 pide_taxi(Mensaje) ->
-    Matriz = matriz(), 
+    Matriz = nodo_servidor(), 
     monitor_node(Matriz, true), 
-    {servidor_taxi, Matriz} ! {self(), Mensaje}, 
+    {servidor, Matriz} ! {self(), Mensaje}, 
     receive
-        {servidor_taxi, Respuesta} ->
-            monitor_node(Matriz, false), 
-            Respuesta;
+        {servidor, {T_PID, NumServicio, Auto, Placa}} ->
+        	io:format("Se le asigno un auto tipo ~p con placas ~p.", [Auto,Placa]).
+        	io:format("Su numero de servicio es: ~p | El T_PID de su taxi es: ~p.", [NumServicio, T_PID]).
+            monitor_node(Matriz, false),
+            receive
+            	{taxi, llega} ->
+            		ok
+            after wait() -> 
+            	cancelar(T_PID)
+            end.
+
+        {servidor, noTaxi} ->
+            noTaxi;
+
         {nodedown, Matriz} ->
-            no
+        	noServer
 end.
 
+cancelar(T_PID) ->
+	Matriz = nodo_taxi(),
+	{taxi, Matriz} ! {self(), cancelar}.
 
+
+wait() -> random:uniform(10000).

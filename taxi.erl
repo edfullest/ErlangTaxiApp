fd4dc1c59bdf2c2af9190452a5b9a25df7637818
Eduardo Serna <lalotahoma@icloud.com>
2017-11-15 19:55:52 -0600

    Fixed syntax bugs in Taxi.erl

--- taxi.erl
+++ taxi.erl
@@ -1,37 +1,38 @@
--module(cliente). 
+-module(taxi). 
 -export([crear_taxi/3, inicio_lista_centrales/0]).
 -import('matriz', [genera_nodo/1]).
 
 % La funcion genera_nodo genera la matriz a donde se conectara, con todo y su hostname
 % En este caso, se genera servidor_taxi@HOSTNAME
-nodo_servidor() -> genera_nodo("central_taxis").
-nodo_cliente() -> genera_nodo("cliente").
+% nodo_servidor() -> genera_nodo("central_taxis").
+% nodo_cliente() -> genera_nodo("cliente").
 
 crear_taxi(Tipo, Placas, NombreCentral)->
-    lista_centrales ! {crear_taxi, {Tipo,Placas,NombreCentral}}
+    lista_centrales ! {crear_taxi, {Tipo,Placas,NombreCentral}}.
 
 % taxi
 servicio_taxi(Tipo,Placas,PID_Central) ->
 		receive
-			{PID_Cliente, {X,Y}} ->
+			{PID_Cliente, _} ->
 				Espera = wait(),
 				io:fwrite("El taxi con placas ~s esta esperando por ~p ~n", [Placas,Espera]),
 				receive 
-					{De , cancelar} ->
-					{PID_Central, nodo_servidor()} ! {respuesta_taxi, servicio_cancelado},
+					{PID_Cliente, cancelar} ->
+                    io:fwrite("El taxi con placas ~s fue cancelado por cliente con PID ~p ~n", [Placas,PID_Cliente]),
+					PID_Central ! {respuesta_taxi, servicio_cancelado},
 					PID_Central ! {nuevo_taxi,self(), Tipo, Placas},
 					servicio_taxi(Tipo,Placas,PID_Central)
 				after Espera ->
 					io:fwrite("El taxi con placas ~s hace servicio ~n", [Placas]),			
-					{pide_taxi,nodo_servidor()} ! {taxi,llega},
-					{PID_Central, servicio_central}! {respuesta_taxi, servicio_ofrecido}
-			
-		end;
+					PID_Cliente ! {taxi,llega},
+					PID_Central ! {respuesta_taxi, servicio_ofrecido}
+                end			
+		end.
 		
 lista_centrales(ListaCentrales) ->
     receive
         {crear_taxi, {Tipo, Placas, NombreCentral}} ->
-			case buscar(Nombre, ListaCentrales) of
+			case busca(NombreCentral, ListaCentrales) of
 				indefinido ->
 					io:fwrite("La central no existe ~n"),
 					lista_centrales(ListaCentrales);
@@ -42,17 +43,17 @@ lista_centrales(ListaCentrales) ->
 			end;
 		{respuesta_central, Nombre, {X,Y}, PID_Central} ->
 				lista_centrales(ListaCentrales ++ {Nombre, {X,Y}, PID_Central})
-    end;
+    end.
 
-wait() -> random:uniform(5000);
+wait() -> rand:uniform(5000).
 
-busca(Nombre, [{Nombre, Location, PID_Central}|_]) ->
-    PID_Central; 
+busca(Nombre, [{Nombre, _, PID_Central}|_]) ->
+    PID_Central;
 busca(Quien, [_|T]) ->
     busca(Quien, T);
 busca(_, _) ->
-    indefinido;
+    indefinido.
 
 inicio_lista_centrales() ->
-    register(lista_centrales, spawn(fun() -> lista_centrales([])end));
+    register(lista_centrales, spawn(fun() -> lista_centrales([])end)).
 

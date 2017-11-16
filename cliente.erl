ce664d4d7cd8effae3ed5492153a4c2f6373c45e
Angel Seiji Morimoto Burgos <angel_seiji@hotmail.com>
2017-11-15 19:48:40 -0600

    Fixed bugs and allowed compatibility between erlang files

--- cliente.erl
+++ cliente.erl
@@ -1,13 +1,13 @@
 -module(cliente). 
--export([pedir_taxi/2]).
+-export([pedir_taxi/2, pide_taxi/2]).
 -import('matriz', [genera_nodo/1]).
 
 % La funcion genera_nodo genera la matriz a donde se conectara, con todo y su hostname
 % En este caso, se genera servidor_taxi@HOSTNAME
 nodo_servidor() -> genera_nodo("servidor_taxi").
-nodo_taxi() -> genera_nodo("taxi").
+% nodo_taxi() -> genera_nodo("taxi").
 pedir_taxi(Quien, {X, Y}) ->
-    pide_taxi(Quien, {X, Y}).
+    spawn(cliente, pide_taxi, [Quien, {X, Y}]).
 
 % cliente 
 pide_taxi(Quien, {X, Y}) ->
@@ -15,9 +15,9 @@ pide_taxi(Quien, {X, Y}) ->
     monitor_node(Matriz, true), 
     {servidor_taxi, Matriz} ! {pedir_cliente, Quien, {X, Y}, self()}, 
     receive
-        {NumServicio, T_PID, Tipo, Placas}} ->
-        	io:format("Se le asigno un auto tipo ~p con placas ~p.", [Tipo, Placas]).
-        	io:format("Su numero de servicio es: ~p | El T_PID de su taxi es: ~p.", [NumServicio, T_PID]).
+        {NumServicio, T_PID, Tipo, Placas} ->
+        	io:format("Se le asigno un auto tipo ~p con placas ~p.", [Tipo, Placas]),
+        	io:format("Su numero de servicio es: ~p | El T_PID de su taxi es: ~p.", [NumServicio, T_PID]),
             monitor_node(Matriz, false),
             receive
             	{taxi, llega} ->
@@ -31,9 +31,6 @@ pide_taxi(Quien, {X, Y}) ->
         	noServer
 end.
 
-cancelar(T_PID) ->
-	Matriz = nodo_taxi(),
-	{taxi, Matriz} ! {self(), cancelar}.
+cancelar(T_PID) -> T_PID ! {self(), cancelar}.
 
-
-wait() -> random:uniform(10000).
+wait() -> rand:uniform(10000).
--- taxi.erl
+++ taxi.erl
@@ -1,62 +1,53 @@
--module(cliente). 
--export([crea_taxi/3, inicio_lista_centrales/0]).
+-module(taxi). 
+-export([crear_taxi/3, inicio_lista_centrales/0]).
 -import('matriz', [genera_nodo/1]).
 
-% La funcion genera_nodo genera la matriz a donde se conectara, con todo y su hostname
-% En este caso, se genera servidor_taxi@HOSTNAME
-nodo_servidor() -> genera_nodo("central_taxis").
-nodo_cliente() -> genera_nodo("cliente").
-
-crea_taxi(Tipo,Placas,NombreCentral) ->
-	PID_taxi = spawn(fun () -> servicio_central(Nombre, [], 0, 0) end)
-    crear_taxi(Tipo, Placas, NombreCentral).
-
 % taxi
-servicio_taxi(Tipo,Placas,PID_Central}) ->
+servicio_taxi(Tipo,Placas,PID_Central) ->
 		receive
-			{PID_Cliente, {X,Y}} ->
+			{PID_Cliente, {_,_}} ->
 				Espera = wait(),
 				io:fwrite("El taxi con placas ~s esta esperando por ~p ~n", [Placas,Espera]),
 				receive 
-					{De , cancelar} ->
-					{servicio_central, PID_Central} ! {respuesta_taxi, servicio_cancelado},
-					PID_Central ! {nuevo_taxi,self(), Tipo, Placas},
-					servicio_taxi(Tipo,Placas,PID_Central)
+					{PID_Cliente, cancelar} ->
+						PID_Central ! {respuesta_taxi, servicio_cancelado},
+						PID_Central ! {nuevo_taxi, {self(), Tipo, Placas}},
+						servicio_taxi(Tipo,Placas,PID_Central)
 				after Espera ->
 					io:fwrite("El taxi con placas ~s hace servicio ~n", [Placas]),			
-					{pide_taxi,PID_Cliente} ! {taxi,llega},
-					{servicio_central, PID_Central} ! {respuesta_taxi, servicio_ofrecido}.
-			
+					PID_Cliente ! {taxi,llega},
+					PID_Central ! {respuesta_taxi, servicio_ofrecido}
+				end
 		end.
 		
 lista_centrales(ListaCentrales) ->
     receive
         {crear_taxi, {Tipo, Placas, NombreCentral}} ->
-			case buscar(Nombre, ListaCentrales) of
+			case buscar(NombreCentral, ListaCentrales) of
 				indefinido ->
 					io:fwrite("La central no existe ~n"),
-					lista_centrales(ListaCentrales).
-				PID_Central ->
-					PID_Taxi = spawn(fun() -> servicio_taxi(Tipo, Placas, PID_Central)),
-					PID_Central ! {nuevo_taxi, {PID_Taxi, Tipo, Placas}},
 					lista_centrales(ListaCentrales);
-			end.
-		{respuesta_central, Nombre, {X,Y}, PID_Central} ->
-				lista_centrales(ListaCentrales ++ {Nombre, {X,Y}, PID_Central})
+				PID_Central ->
+					PID_Taxi = spawn(fun() -> servicio_taxi(Tipo, Placas, PID_Central) end),
+					PID_Central ! {nuevo_taxi, {PID_Taxi, Tipo, Placas}},
+					lista_centrales(ListaCentrales)
+			end;
+		{respuesta_central, NombreCentral, {X,Y}, PID_Central} ->
+			lista_centrales(ListaCentrales ++ [{NombreCentral, {X,Y}, PID_Central}])
     end.
 
-wait() -> random:uniform(5000).
+wait() -> rand:uniform(5000).
 
-busca(Nombre, [{Nombre, Location, PID_Central}|_]) ->
+buscar(Nombre, [{Nombre, _, PID_Central}|_]) ->
     PID_Central; 
-busca(Quien, [_|T]) ->
-    busca(Quien, T);
-busca(_, _) ->
+buscar(Quien, [_|T]) ->
+    buscar(Quien, T);
+buscar(_, _) ->
     indefinido.
 	
 crear_taxi(Tipo, Placas, NombreCentral)->
     lista_centrales ! {crear_taxi, {Tipo,Placas,NombreCentral}}.
 
 inicio_lista_centrales() ->
-    register(lista_centrales, spawn(fun() -> lista_centrales([]))).
+    register(lista_centrales, spawn(fun() -> lista_centrales([]) end)).
 

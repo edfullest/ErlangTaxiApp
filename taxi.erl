ad226989e31cfb6f346cbd991232eaaea4e6e2d8
JuanOrdaz <juan_ordaz52@hotmail.com>
2017-11-15 19:43:58 -0600

    se modifica v2 Taxi

--- taxi.erl
+++ taxi.erl
@@ -1,5 +1,5 @@
 -module(cliente). 
--export([crea_taxi/3, inicio_lista_centrales/0]).
+-export([crear_taxi/3, inicio_lista_centrales/0]).
 -import('matriz', [genera_nodo/1]).
 
 % La funcion genera_nodo genera la matriz a donde se conectara, con todo y su hostname
@@ -7,12 +7,11 @@
 nodo_servidor() -> genera_nodo("central_taxis").
 nodo_cliente() -> genera_nodo("cliente").
 
-crea_taxi(Tipo,Placas,NombreCentral) ->
-	PID_taxi = spawn(fun () -> servicio_central(Nombre, [], 0, 0) end)
-    crear_taxi(Tipo, Placas, NombreCentral).
+crear_taxi(Tipo, Placas, NombreCentral)->
+    lista_centrales ! {crear_taxi, {Tipo,Placas,NombreCentral}};
 
 % taxi
-servicio_taxi(Tipo,Placas,PID_Central}) ->
+servicio_taxi(Tipo,Placas,PID_Central) ->
 		receive
 			{PID_Cliente, {X,Y}} ->
 				Espera = wait(),
@@ -25,9 +24,9 @@ servicio_taxi(Tipo,Placas,PID_Central}) ->
 				after Espera ->
 					io:fwrite("El taxi con placas ~s hace servicio ~n", [Placas]),			
 					{pide_taxi,nodo_servidor()} ! {taxi,llega},
-					{PID_Central, servicio_central}! {respuesta_taxi, servicio_ofrecido}.
+					{PID_Central, servicio_central}! {respuesta_taxi, servicio_ofrecido}
 			
-		end.
+		end;
 		
 lista_centrales(ListaCentrales) ->
     receive
@@ -35,28 +34,25 @@ lista_centrales(ListaCentrales) ->
 			case buscar(Nombre, ListaCentrales) of
 				indefinido ->
 					io:fwrite("La central no existe ~n"),
-					lista_centrales(ListaCentrales).
-				PID_Central ->
-					PID_Taxi = spawn(fun() -> servicio_taxi(Tipo, Placas, PID_Central)),
-					PID_Central ! {nuevo_taxi, {PID_Taxi, Tipo, Placas}},
 					lista_centrales(ListaCentrales);
-			end.
+				PID_Central ->
+					PID_Taxi = spawn(fun() -> servicio_taxi(Tipo, Placas, PID_Central)end),
+					PID_Central ! {nuevo_taxi, {PID_Taxi, Tipo, Placas}},
+					lista_centrales(ListaCentrales)
+			end;
 		{respuesta_central, Nombre, {X,Y}, PID_Central} ->
 				lista_centrales(ListaCentrales ++ {Nombre, {X,Y}, PID_Central})
-    end.
+    end;
 
-wait() -> random:uniform(5000).
+wait() -> random:uniform(5000);
 
 busca(Nombre, [{Nombre, Location, PID_Central}|_]) ->
     PID_Central; 
 busca(Quien, [_|T]) ->
     busca(Quien, T);
 busca(_, _) ->
-    indefinido.
-	
-crear_taxi(Tipo, Placas, NombreCentral)->
-    lista_centrales ! {crear_taxi, {Tipo,Placas,NombreCentral}}.
+    indefinido;
 
 inicio_lista_centrales() ->
-    register(lista_centrales, spawn(fun() -> lista_centrales([]))).
+    register(lista_centrales, spawn(fun() -> lista_centrales([])end));
 
